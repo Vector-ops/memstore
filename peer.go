677578ff1f48ae2d15ee775c7322a2c237abe4fd
Vector-ops <sumith2827@gmail.com>
2024-08-07 20:23:46 +0530

    refact: move command parsing into read loop
    move command parsing to peer read loop from proto

--- peer.go
+++ peer.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"log"
 	"net"
+
+	"github.com/tidwall/resp"
 )
 
 type Peer struct {
@@ -21,17 +26,60 @@ func NewPeer(conn net.Conn, msgCh chan Message) *Peer {
 }
 
 func (p *Peer) readLoop() error {
-	buf := make([]byte, 1024)
+	// buf := make([]byte, 1024)
+	// for {
+	// 	n, err := p.conn.Read(buf)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	msgBuf := make([]byte, n)
+	// 	copy(msgBuf, buf[:n])
+	// 	p.msgCh <- Message{
+	// 		data: msgBuf,
+	// 		peer: p,
+	// 	}
+	// }
+
+	rd := resp.NewReader(p.conn)
 	for {
-		n, err := p.conn.Read(buf)
-		if err != nil {
-			return err
+		v, _, err := rd.ReadValue()
+		if err == io.EOF {
+			break
 		}
-		msgBuf := make([]byte, n)
-		copy(msgBuf, buf[:n])
-		p.msgCh <- Message{
-			data: msgBuf,
-			peer: p,
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if v.Type() == resp.Array {
+			for _, c := range v.Array() {
+				switch c.String() {
+				case CommandGET:
+					if len(v.Array()) != 2 {
+						return fmt.Errorf("invalid number of variables for GET command")
+					}
+					cmd := GetCommand{
+						key: v.Array()[1].Bytes(),
+					}
+					p.msgCh <- Message{
+						cmd:  cmd,
+						peer: p,
+					}
+				case CommandSET:
+					if len(v.Array()) != 3 {
+						return fmt.Errorf("invalid number of variables for SET command")
+					}
+					cmd := SetCommand{
+						key:   v.Array()[1].Bytes(),
+						value: v.Array()[2].Bytes(),
+					}
+					p.msgCh <- Message{
+						cmd:  cmd,
+						peer: p,
+					}
+				default:
+				}
+			}
 		}
 	}
+	return nil
 }
