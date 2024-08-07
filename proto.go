52ccc8a4fb7b9f9a66f3d02a41275b27bf754629
Vector-ops <sumith2827@gmail.com>
2024-08-07 19:29:43 +0530

    feat: handle GET command
    add GetCommand struct
    handle GET command case

--- proto.go
+++ proto.go
@@ -11,12 +11,16 @@ import (
 
 const (
 	CommandSET = "SET"
+	CommandGET = "GET"
 )
 
 type Command interface {
 }
 
 type SetCommand struct {
+	key, value []byte
+}
+type GetCommand struct {
 	key, value []byte
 }
 
@@ -34,9 +38,17 @@ func parseCommand(raw string) (Command, error) {
 		if v.Type() == resp.Array {
 			for _, c := range v.Array() {
 				switch c.String() {
+				case CommandGET:
+					if len(v.Array()) != 2 {
+						return nil, fmt.Errorf("invalid number of variables for GET command")
+					}
+					cmd := GetCommand{
+						key: v.Array()[1].Bytes(),
+					}
+					return cmd, nil
 				case CommandSET:
 					if len(v.Array()) != 3 {
-						return nil, fmt.Errorf("invalid number of variables for set command")
+						return nil, fmt.Errorf("invalid number of variables for SET command")
 					}
 					cmd := SetCommand{
 						key:   v.Array()[1].Bytes(),
