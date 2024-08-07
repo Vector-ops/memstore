1d5207b4a8ce1eaab207502e3ead0e809cda7b02
Vector-ops <sumith2827@gmail.com>
2024-08-07 19:30:09 +0530

    feat: implement GET command

--- client/client.go
+++ client/client.go
@@ -3,7 +3,7 @@ package client
 import (
 	"bytes"
 	"context"
-	"log"
+	"io"
 	"net"
 
 	"github.com/tidwall/resp"
@@ -11,21 +11,19 @@ import (
 
 type Client struct {
 	addr string
-	conn net.Conn
 }
 
 func New(address string) *Client {
-	conn, err := net.Dial("tcp", address)
-	if err != nil {
-		log.Fatal(err)
-	}
 	return &Client{
 		addr: address,
-		conn: conn,
 	}
 }
 
 func (c *Client) Set(ctx context.Context, key string, val string) error {
+	conn, err := net.Dial("tcp", c.addr)
+	if err != nil {
+		return err
+	}
 	buf := &bytes.Buffer{}
 	wr := resp.NewWriter(buf)
 	wr.WriteArray([]resp.Value{
@@ -33,7 +31,34 @@ func (c *Client) Set(ctx context.Context, key string, val string) error {
 		resp.StringValue(key),
 		resp.StringValue(val),
 	})
-	_, err := c.conn.Write(buf.Bytes())
+	_, err = io.Copy(conn, buf)
+	if err != nil {
+		return err
+	}
 
-	return err
+	return nil
+}
+
+func (c *Client) Get(ctx context.Context, key string) (string, error) {
+	conn, err := net.Dial("tcp", c.addr)
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	wr := resp.NewWriter(&buf)
+	wr.WriteArray([]resp.Value{
+		resp.StringValue("GET"),
+		resp.StringValue(key),
+	})
+	_, err = conn.Write(buf.Bytes())
+	if err != nil {
+		return "", err
+	}
+
+	b := make([]byte, 1024)
+	n, err := conn.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return string(b[:n]), nil
 }
