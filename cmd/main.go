8eb7925cc1da7a48499f66cd14578a36b9d76788
vector-ops <85575949+vector-ops@users.noreply.github.com>
2025-12-28 21:24:31 +0530

    update flags for leader address and tcp address

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"log"
-	"os"
+	"time"
 
 	"github.com/vector-ops/memstore/internal/config"
 	"github.com/vector-ops/memstore/internal/server"
@@ -14,48 +13,65 @@ func main() {
 	var nodeId string
 	flag.StringVar(&nodeId, "node-id", "", "Specify the node id to start")
 
-	var configPath string
-	flag.StringVar(&configPath, "config", "config.json", "Path to config file")
-	flag.Parse()
+	// var configPath string
+	// flag.StringVar(&configPath, "config", "", "Path to config file")
+	// flag.Parse()
 
-	if nodeId == "" {
-		log.Fatalf("-node-id not provided")
+	var tcpAddr string
+	flag.StringVar(&tcpAddr, "tcp", "", "Specify the tcp address for the server")
+
+	var leaderAddr string
+	flag.StringVar(&leaderAddr, "leader", "", "Specify the leader address, leave blank if leader")
+
+	var replicas int
+	flag.IntVar(&replicas, "replicas", 0, "Specify the no. of replicas")
+
+	var replicaTimeout int
+	flag.IntVar(&replicaTimeout, "replica-timeout", 0, "Specify the replica connect timeout in milliseconds")
+
+	var replicationMaxRetries int
+	flag.IntVar(&replicationMaxRetries, "max-retries", 0, "Specify the max replication retries")
+
+	var pollInterval int
+	flag.IntVar(&pollInterval, "poll", 0, "Specify the leader poll interval in milliseconds")
+
+	cfg := config.ServerConfig{
+		Id:                    nodeId,
+		TCPAddr:               tcpAddr,
+		LeaderAddr:            leaderAddr,
+		Replicas:              replicas,
+		ReplicationTimeout:    time.Duration(replicaTimeout) * time.Millisecond,
+		ReplicationMaxRetries: replicationMaxRetries,
+		PollInterval:          time.Duration(pollInterval) * time.Millisecond,
 	}
 
-	f, err := os.Open(configPath)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// if configPath != "" {
+	// 	cfg = parseConfig(configPath)
+	// } else {
 
-	var nodes []config.NodeConfig
-	if err = json.NewDecoder(f).Decode(&nodes); err != nil {
-		log.Fatal(err)
-	}
+	// }
 
-	var serverConfig config.NodeConfig
-	replicas := make([]config.NodeConfig, 0)
-	for _, n := range nodes {
-		if n.Id == nodeId {
-			serverConfig = n
-		}
-	}
-
-	if serverConfig.Id == "" {
-		log.Fatalf("node config not found")
-	}
-
-	for _, n := range nodes {
-		if n.Id != nodeId {
-			if serverConfig.Role == "master" {
-				replicas = append(replicas, n)
-			}
-		}
-	}
-
-	server := server.NewServer(server.Config{
-		NodeConfig: serverConfig,
-		Replicas:   replicas,
-	})
+	server := server.NewServer(cfg)
 
 	log.Fatal(server.Start())
 }
+
+// func parseConfig(p string) config.ServerConfig {
+
+// 	var cfg config.ServerConfig
+
+// 	f, err := os.Open(p)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	b, err := io.ReadAll(f)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	_ = b
+
+// 	return cfg
+
+// }
