38eb5b1dd96689bbcdfab6113ab15059f7400582
vector-ops <85575949+vector-ops@users.noreply.github.com>
2025-12-28 20:09:51 +0530

    add node struct that handles node related operations

---
+++ internal/cluster/node.go
@@ -0,0 +1,56 @@
+package cluster
+
+import (
+	"github.com/vector-ops/memstore/internal/server"
+	"github.com/vector-ops/memstore/internal/transport"
+)
+
+type Role string
+
+const (
+	FOLLOWER  Role = "follower"
+	CANDIDATE Role = "candidate"
+	LEADER    Role = "leader"
+)
+
+type Node struct {
+	server           *server.Server // database server used for current node
+	ID               string
+	ConnectionString string
+	Transport        transport.Transport // replica transport
+	Role             Role
+	stopChan         chan bool
+}
+
+func NewNode(id, connectionString string, role Role, t transport.Transport) *Node {
+	return &Node{
+		ID:               id,
+		ConnectionString: connectionString,
+		Role:             role,
+		Transport:        t,
+	}
+}
+
+func (n *Node) SetServer(s *server.Server) {
+	n.server = s
+}
+
+func (n *Node) SetRole(r Role) {
+	n.Role = r
+}
+
+func (n *Node) startHeartBeat() {
+	n.stopChan = make(chan bool)
+	for {
+		select {
+		case <-n.stopChan:
+			return
+		default:
+			n.Transport.Ping()
+		}
+	}
+}
+
+func (n *Node) stopHeartBeat() {
+	close(n.stopChan)
+}
