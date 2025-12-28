176ced0be0dcf0056842cc39fa7d872efe492edf
vector-ops <85575949+vector-ops@users.noreply.github.com>
2025-12-28 20:11:19 +0530

    add http transport

---
+++ internal/transport/http_transport.go
@@ -0,0 +1,41 @@
+package transport
+
+type HttpTransport struct {
+	connectionStr string
+}
+
+func NewHttpTransport(connectionStr string) Transport {
+	return &HttpTransport{
+		connectionStr: connectionStr,
+	}
+}
+
+// GetLocalAddress implements [Transport].
+func (h *HttpTransport) GetLocalAddress() string {
+	return h.connectionStr
+}
+
+// GetRemoteAddress implements [Transport].
+func (h *HttpTransport) GetRemoteAddress() string {
+	return h.connectionStr
+}
+
+// Ping implements [Transport].
+func (h *HttpTransport) Ping() (string, error) {
+	panic("unimplemented")
+}
+
+// Read implements [Transport].
+func (h *HttpTransport) Read() ([]byte, error) {
+	panic("unimplemented")
+}
+
+// ReadLoop implements [Transport].
+func (h *HttpTransport) ReadLoop() error {
+	panic("unimplemented")
+}
+
+// Send implements [Transport].
+func (h *HttpTransport) Send([]byte) (int, error) {
+	panic("unimplemented")
+}
