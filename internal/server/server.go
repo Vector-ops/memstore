855d04e15ad77bf8ebd469d38416e8fa43f5158e
vector-ops <85575949+vector-ops@users.noreply.github.com>
2026-01-03 23:22:47 +0530

    implement heartbeat monitoring when follower

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,7 +3,9 @@ package server
 
 import (
 	"bytes"
+	"log"
 	"log/slog"
+	"math/rand"
 	"net"
 	"sync"
 	"time"
@@ -16,9 +18,9 @@ import (
 )
 
 type ServerConfig struct {
-	ServerAddr  string
-	ClusterAddr string
-	LeaderAddr  string
+	ServerAddr string
+	// ClusterAddr string
+	LeaderAddr string
 
 	// leader config
 	HeartbeatInterval     time.Duration
@@ -32,10 +34,11 @@ type ServerConfig struct {
 
 type Server struct {
 	// server internals
-	id  uuid.UUID
-	cfg ServerConfig
-	mu  *sync.Mutex
-	wg  *sync.WaitGroup
+	id       uuid.UUID
+	cfg      ServerConfig
+	hbTicker *time.Ticker
+	mu       *sync.Mutex
+	wg       *sync.WaitGroup
 
 	// network
 	peers     map[transport.Transport]bool
@@ -56,6 +59,27 @@ type Server struct {
 func NewServer(cfg ServerConfig) *Server {
 
 	clstrMgr := cluster.NewClusterManager(cluster.FOLLOWER, cfg.ReplicaCount)
+
+	// validate config
+	if cfg.HeartbeatInterval == 0 {
+		cfg.HeartbeatInterval = time.Millisecond * 50
+	}
+
+	if cfg.HeartbeatTimeout == 0 {
+		cfg.HeartbeatTimeout = time.Millisecond * time.Duration(rand.Int63n(300))
+	}
+
+	if cfg.ReplicationMaxRetries == 0 {
+		cfg.ReplicationMaxRetries = 2
+	}
+
+	if cfg.ReplicationTimeout == 0 {
+		cfg.ReplicationTimeout = time.Second * 5
+	}
+
+	if cfg.ServerAddr == "" {
+		log.Fatalf("server address not found in config")
+	}
 
 	return &Server{
 		id:        uuid.New(),
@@ -80,6 +104,10 @@ func (s *Server) Start() error {
 		return err
 	}
 	s.ln = ln
+
+	if s.role == cluster.FOLLOWER {
+		s.hbTicker = time.NewTicker(s.cfg.HeartbeatTimeout)
+	}
 
 	go s.loop()
 
@@ -122,18 +150,22 @@ func (s *Server) handleMsg(msg transport.Message) error {
 		}
 
 	case protocol.PingCommand:
-		var buf bytes.Buffer
-		if _, err := buf.WriteString(s.id.String()); err != nil {
-			slog.Error("Failed to write to buffer", "err", err)
-		}
-		if err := buf.WriteByte('\n'); err != nil {
-			slog.Error("Failed to write to buffer", "err", err)
-		}
+		s.hbTicker.Reset(s.cfg.HeartbeatTimeout)
+		log.Println("Recieved heartbeat resetting timer")
+		// msg.Transport.Close()
+		// log.Println("Closed safely")
+		// var buf bytes.Buffer
+		// if _, err := buf.WriteString(s.id.String()); err != nil {
+		// 	slog.Error("Failed to write to buffer", "err", err)
+		// }
+		// if err := buf.WriteByte('\n'); err != nil {
+		// 	slog.Error("Failed to write to buffer", "err", err)
+		// }
 
-		_, err := msg.Transport.Send(buf.Bytes())
-		if err != nil {
-			slog.Error("peer send eror", "err", err)
-		}
+		// _, err := msg.Transport.Send(buf.Bytes())
+		// if err != nil {
+		// 	slog.Error("peer send eror", "err", err)
+		// }
 	}
 	return nil
 }
@@ -145,7 +177,15 @@ func (s *Server) loop() {
 			if err := s.handleMsg(msg); err != nil {
 				slog.Error("raw message error", "err", err)
 			}
+		case t := <-s.hbTicker.C:
+			// TODO: send vote request with the new term to all the nodes
+			if s.role == cluster.FOLLOWER {
+				s.role = cluster.CANDIDATE
+				log.Println("Heartbeat timedout: turning into candidate", t)
+				s.hbTicker.Reset(s.cfg.HeartbeatTimeout)
+			}
 		case <-s.quitCh:
+			s.Stop()
 			return
 		case peer := <-s.addPeerCh:
 			slog.Info("new peer connected", "remoteAddr", peer.GetRemoteAddress())
@@ -176,4 +216,21 @@ func (s *Server) handleConn(conn net.Conn) {
 	if err := peer.ReadLoop(); err != nil {
 		slog.Error("peer read error", "err", err, "remoteAddr", conn.RemoteAddr())
 	}
+}
+
+func (s *Server) StartHearbeat() {
+	// TODO: send hearbeat ping to all nodes in the cluster
+}
+
+func (s *Server) stopHeartbeat() {}
+
+func (s *Server) Stop() {
+	s.hbTicker.Stop()
+	s.ln.Close()
+
+	close(s.addPeerCh)
+	close(s.delPeerCh)
+	close(s.msgCh)
+	close(s.quitCh)
+
 }
